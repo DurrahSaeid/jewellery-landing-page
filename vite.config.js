df92ce84d32a31d284241e76c9d695a260343a1d
DurrahSaeid <167574333+DurrahSaeid@users.noreply.github.com>
2026-08-23 20:52:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      "durrah-jewellery-landing-page-durrah-saeid-2005-dev.apps.rm2.thpm.p1.openshiftapps.com"
+      ]
+  }
 });
